a186d3ca7dc5978800a8b77c2027aa3c70e93fed
rise42 <makedonskiy2500@gmail.com>
2018-04-28 13:29:40 +0300

    Date validation + getting rid of the warning

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import axios from 'axios'
+import moment from 'moment'
 
 import variables from './variables.js'
 
@@ -17,28 +18,28 @@ async function main () {
   }
 
   const dataset = request.data.data
-  const putURL = 'http://' + variables.DB_SERVICE_HOST + ':' + variables.DB_SERVICE_PORT + '/' + request.data.name
+  const dbURL = 'http://' + variables.DB_SERVICE_HOST + ':' + variables.DB_SERVICE_PORT + '/' + request.data.name
 
-  setInterval(dataRequest, variables.INTERVAL * 1000, putURL, dataset)
+  setInterval(dataRequest, variables.INTERVAL * 1000, dbURL, dataset)
 }
 
-async function dataRequest (putURL, dataset) {
+async function dataRequest (dbURL, dataset) {
   const newRequest = await axios.get(dataset.get.url)
   const newdata = newRequest.data
 
   const newValue = Number(parse(newdata, dataset.get.price).replace(',', ''))
-  const newTime = adjustTimeFormat(parse(newdata, dataset.get.timestamp), dataset.get.timeformat)
+  const newTime = getTime(parse(newdata, dataset.get.timestamp))
 
-  const oldRequest = await axios.get(putURL + '/lastInsert')
-  const oldTime = new Date(oldRequest.data).getTime()
+  const oldRequest = await axios.get(dbURL + '/lastInsert')
+  const oldTime = moment(oldRequest.data)
 
-  if (newTime > oldTime) {
+  if (newTime.isAfter(oldTime)) {
     console.log('Puttin\' in new value: ' + newValue + ' at ' + newTime)
     await axios.put(
-      putURL,
+      dbURL,
       {
         value: newValue,
-        timestamp: newTime
+        timestamp: newTime.format()
       }
     )
   }
@@ -46,14 +47,12 @@ async function dataRequest (putURL, dataset) {
 
 function parse (data, sequence) {
   let obj = data
-  sequence.forEach((element) => obj = obj[element])
+  sequence.forEach(element => obj = obj[element])
   return obj
 }
 
-function adjustTimeFormat (rawTime, timeformat) {
-  if (timeformat === 'unix') {
-    return new Date(rawTime * 1e3).getTime()
-  } else if (timeformat === 'mills') {
-    return new Date(rawTime).getTime()
-  }
+function getTime (parsedValue) {
+  if (parsedValue.length === 10) {
+    return moment(Number(parsedValue * 1000))
+  } else return moment(parsedValue)
 }
--- src/variables.js
+++ src/variables.js
@@ -1,9 +1,9 @@
 export default ['DB_SERVICE_HOST', 'DB_SERVICE_PORT', 'DATASET', 'INTERVAL', 'CONFIG_HOST', 'CONFIG_PORT']
-.reduce((acc, val) => {
-  if (process.env[val]) {
-    acc[val] = process.env[val]
-    return acc
-  } else {
-    throw new Error('Enviromental property ' + val + ' is missing')
-  }
-}, {})
+  .reduce((acc, val) => {
+    if (process.env[val]) {
+      acc[val] = process.env[val]
+      return acc
+    } else {
+      throw new Error('Enviromental property ' + val + ' is missing')
+    }
+  }, {})
